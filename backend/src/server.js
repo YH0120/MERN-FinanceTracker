3de353b7cd78841927d8444ea790fea38797f819
YH0120 <terh2002@gmail.com>
2026-02-15 19:12:30 +0800

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors'
 import cookieParser from "cookie-parser";
-
+import path from "path"
 import transactionRoutes from "./routes/transactionRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -14,14 +14,18 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 // middleware
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
-);
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+            credentials: true,
+        })
+    );
+}
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(rateLimiter);
@@ -30,6 +34,14 @@ app.use(rateLimiter);
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/categories", categoryRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
